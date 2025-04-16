6ca0b9c2ab57f27c479fb879a60d3adc848a89fd
daekwon <pranludi@gmail.com>
2025-04-17 01:31:47 +0900

    fix

--- src/test/java/io/pranludi/crossfit/member/MemberServiceApplicationTests.java
+++ src/test/java/io/pranludi/crossfit/member/MemberServiceApplicationTests.java
@@ -1,13 +1,15 @@
 package io.pranludi.crossfit.member;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MemberServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        Assertions.assertThat(10).isEqualTo(10);
+    }
 
 }
