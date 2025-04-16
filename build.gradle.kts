10daaeac5b25cad8a93555ecad0bbf29f969889a
daekwon <pranludi@gmail.com>
2025-04-17 01:04:53 +0900

    kafka

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,6 +45,9 @@ dependencies {
   runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
   runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
+  implementation("com.fasterxml.jackson.core:jackson-databind:2.18.3")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.3")
+
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.springframework.grpc:spring-grpc-test")
   testImplementation("org.springframework.kafka:spring-kafka-test")
